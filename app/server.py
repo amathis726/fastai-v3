c379258aff8b289e05612be8e7190fc8e3127886
amathis726 <44485445+amathis726@users.noreply.github.com>
2019-02-08 15:50:23 -0800

    Update server.py
    Added dropbox download link to export.pkl.
    Added classes for birds of prey predictor

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/bsj7zl976en8os3/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['eagle', 'osprey', 'falcon', 'red kite', 'harpy eagle', 'hawk', 'kestrel', 'owl']
 path = Path(__file__).parent
 
 app = Starlette()
